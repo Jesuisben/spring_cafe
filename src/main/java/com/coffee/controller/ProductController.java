2cb9f8ee91d2d5ded5329ac23b89d764a053a4d8
Jesuisben <rktngusals@gmail.com>
2026-04-10 17:36:23 +0900

    2026.04.10(금)

--- src/main/java/com/coffee/controller/ProductController.java
+++ src/main/java/com/coffee/controller/ProductController.java
@@ -2,24 +2,30 @@ package com.coffee.controller;
 
 import com.coffee.entity.Product;
 import com.coffee.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
     @Autowired
-    private ProductService productService ;
+    private ProductService productService;
 
     @GetMapping("/list")
-    public List<Product> list(){
-        List<Product> products = this.productService.getProductList() ;
-        return products ;
+    public List<Product> list() {
+        List<Product> products = this.productService.getProductList();
+        return products;
     }
 
     // {id}를 경로 변수라고 부르며, 가변 매개 변수라고 부름
@@ -27,23 +33,86 @@ public class ProductController {
     // 경로변수를 delete() 안에 넣고 @PathVariable이라는 경로변수 어노테이션도 작성하면
     // 매핑에 있는 경로 변수 id가 delete의 id로 들어옴
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> delete(@PathVariable Long id){
+    public ResponseEntity<String> delete(@PathVariable Long id) {
         try {
             boolean isDeleted = this.productService.deleteProduct(id);
 
-            if(isDeleted){
+            if (isDeleted) {
                 // 여기까지 왔다는 것은 에러없이 성공해서 내가 원하는 동작인 삭제가 된 것임
                 return ResponseEntity.ok(id + "번 상품이 삭제되었습니다.");
-            }else{ // 삭제를 할 상품 자체가 없는 상태
+            } else { // 삭제를 할 상품 자체가 없는 상태
                 return ResponseEntity.badRequest().body(id + "번 상품이 존재하지 않습니다.");
             }
 
-        }catch (DataIntegrityViolationException err){// 데이터 베이스 무결성 위배되는 에러가 발생할때
-            String message = "해당 상품은 장바구니에 포함이 되어 있거나, 이미 매출이 발생한 상품입니다. \n확인해 주세요." ;
+        } catch (DataIntegrityViolationException err) {// 데이터 베이스 무결성 위배되는 에러가 발생할때
+            String message = "해당 상품은 장바구니에 포함이 되어 있거나, 이미 매출이 발생한 상품입니다. \n확인해 주세요.";
             return ResponseEntity.internalServerError().body(message);
 
-        }catch (Exception err){ // 두루뭉실한 예외처리
+        } catch (Exception err) { // 두루뭉실한 예외처리
             return ResponseEntity.internalServerError().body("오류 발생 : " + err.getMessage());
+
+        }
+
+
+    }
+
+    @PostMapping("/insert")
+    public ResponseEntity<?> insert(@Valid @RequestBody Product product, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) { // bindingResult에 문제가 있으면
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError xx : bindingResult.getFieldErrors()) {
+                errors.put(xx.getField(), xx.getDefaultMessage());
+            }
+
+            return new ResponseEntity<>(
+                    Map.of(
+                            "message", "상품 등록 유효성 검사에 문제가 있습니다.",
+                            "errors", errors
+                    ),
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+
+        try { // 성공하면
+            Product savedProduct = this.productService.insertProduct(product);
+
+            if (savedProduct == null) {
+                return ResponseEntity
+                        .status(500)
+                        .body(
+                                Map.of(
+                                        "message", "상품 등록에 실패하였습니다.",
+                                        "error", "bad image file format"
+                                )
+                        );
+            }
+
+            return ResponseEntity.ok(
+                    Map.of(
+                            "message", "상품이 성공적으로 등록되었습니다.",
+                            "image", savedProduct.getImage()
+                    )
+            );
+
+        } catch (IllegalStateException err) { // 경로 또는 이미지 저장 문제
+            return ResponseEntity
+                    .status(500)
+                    .body(
+                            Map.of(
+                                    "message", err.getMessage(),
+                                    "error", "File Save Error"
+                            )
+                    );
+
+        } catch (Exception err) { // 데이터 베이스 오류
+            return ResponseEntity
+                    .status(500)
+                    .body(
+                            Map.of(
+                                    "message", err.getMessage(),
+                                    "error", "Internet Server Error"
+                            )
+                    );
 
         }
     }
--- src/main/java/com/coffee/service/ProductService.java
+++ src/main/java/com/coffee/service/ProductService.java
@@ -7,20 +7,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
-    private ProductRepository productRepository ;
+    private ProductRepository productRepository;
 
-    public List<Product> getProductList(){
+    public List<Product> getProductList() {
         return this.productRepository.findProductByOrderByIdDesc();
     }
 
     // application.properties에서 이미지가 있는 실제 위치 가져오기
     @Value("${productImageLocation}")
-    private String productImageLocation ;
+    private String productImageLocation;
 
     // 상품 id를 이용한 삭제
     // 1) 삭제할 상품이 데이터 베이스에 실제로 존재하는지 id를 통해 확인
@@ -28,35 +33,86 @@ public class ProductService {
     // 3) 상품 자체를 삭제
 
     // 1) 상품이 존재 하는지 확인
-    public boolean deleteProduct(Long id){
+    public boolean deleteProduct(Long id) {
         // 상품 id가져오는데 없으면 없네~하고싶은데 오류가 나니까 orElse넣기
-        Product product = productRepository.findById(id).orElse(null) ;
+        Product product = productRepository.findById(id).orElse(null);
 
         // 상품이 무의미하면, 없다면
-        if(product == null){
-            return false ;
+        if (product == null) {
+            return false;
         }
 
         // 상품이 유의미하면, 있다면
         // 2) 이미지가 있는 c드라이브 폴더(운영체제 내)에 가서 이미지 지우기
-        String fileName = product.getImage() ;
-        if(fileName != null && !fileName.isEmpty()){
+        String fileName = product.getImage();
+        if (fileName != null && !fileName.isEmpty()) {
             // application.properties에서 가져온 이미지 위치 연결하기
-            File file = new File(productImageLocation + fileName) ;
+            File file = new File(productImageLocation + fileName);
 
             System.out.println("삭제될 파일 이름");
             System.out.println(file.getAbsolutePath()); // 절대 경로 보여주기
 
-            if(file.exists()){
-              boolean deleted = file.delete() ;
+            if (file.exists()) {
+                boolean deleted = file.delete();
 
-              if(!deleted){
-                  System.out.println("이미지 삭제 실패");
-              }
+                if (!deleted) {
+                    System.out.println("이미지 삭제 실패");
+                }
             }
         }
         // 3) 데이터 베이스에서 상품 삭제하기
         productRepository.deleteById(id);
         return true;
     }
+
+    // 리액트에서 const reader = new FileReader();로 이미지를 텍스트로 변환함
+    // 리액트에서 이미지를 텍스트로 변환한 그 텍스트가 base64Image로 들어옴
+    private String saveProductImage(String base64Image) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        String formattedNow = LocalDateTime.now().format(formatter);
+        // UUID 클래스 공부 (Universal Unique ID)
+        String imageFileName = "product_" + formattedNow + ".jpg";
+
+        File imageFile = new File(productImageLocation + imageFileName);
+        System.out.println("등록할 이미지 이름");
+        System.out.println(imageFile.getAbsolutePath());
+
+        // byte인 이미지 파일을 문자로 바꾸고 그 문자를 나누고
+        byte[] decodedImage = Base64.getDecoder().decode(base64Image.split(",")[1]);
+
+        try {
+            // 실제 저장은 byte로 저장함
+            FileOutputStream fos = new FileOutputStream(imageFile);
+            fos.write(decodedImage); // c:\shop\images 폴더에 복사되는 것처럼 보이지만 write를 씀
+            return imageFileName;
+
+        } catch (Exception e) {
+            String message = "이미지 파일 저장중에 오류가 발생했습니다.";
+            throw new IllegalStateException(message);
+        }
+
+    }
+
+
+    public Product insertProduct(Product product) {
+        // product는 리액트에서 넘어온 상품 등록을 위항 정보입니다.
+        if (product.getImage() != null && product.getImage().startsWith("data:image")) {
+            String imageFileName = this.saveProductImage(product.getImage());
+
+            // 데이터 베이스에는 product_년월일시분초.jpg 형식으로 저장되어야 합니다.
+            product.setImage(imageFileName);
+
+            product.setInputdate(LocalDate.now());
+            System.out.println("서비스 클래스에서 상품 등록 정보 확인");
+            System.out.println(product);
+
+            // 메소드 명은 save인데 실제 데이터 베이스에 추가하니까 사실상 SQL의 insert임
+            return this.productRepository.save(product); // 데이터 베이스에 추가하기
+        } else {
+            return null;
+        }
+
+
+    }
+
 }
